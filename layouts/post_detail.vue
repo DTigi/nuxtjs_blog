de496aa4abe50cb284bea3506602de1884a7afd5
DTigi <dtiginyanu@gmail.com>
2025-02-04 17:18:57 +0300

    add SEO (tags: canonical , title, description, keywords), schema.org and Twitter and Open Graph

--- layouts/post_detail.vue
+++ layouts/post_detail.vue
@@ -1,24 +1,29 @@
 <template>
-    <div>
-      <Navbar />
-      <Nuxt />
-      <Footer />
-    </div>
-  </template>
-  
-  
-  <script>
-  import Navbar from "~/components/Navbar";
-  import Footer from "~/components/Footer";
-  
-  export default {
-    components: {
-      Footer,
-      Navbar,
+  <div>
+    <Navbar />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+
+<script>
+import Navbar from "~/components/Navbar";
+import Footer from "~/components/Footer";
+
+export default {
+  components: {
+    Footer,
+    Navbar,
+  },
+  head() {
+    return {
+      link: [
+        { rel: "canonical", href: `http://localhost:3000${this.$route.path}` }
+      ]
     }
-  };
-  </script>
-  
-  <style>
-  
-  </style>
+  },
+};
+</script>
+
+<style></style>
